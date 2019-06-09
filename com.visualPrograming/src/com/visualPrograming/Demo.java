ec1f5f63b99afd6482cb14c2e2a6a6e0f9c8745c
Ashley <macbookpro@Macintosh.local>
2019-06-09 12:50:02 -0400

    [Jallal] added Demo class

---
+++ com.visualPrograming/src/com/visualPrograming/Demo.java
@@ -0,0 +1,15 @@
+package com.visualPrograming;
+
+public class Demo {
+
+	private int a = 3;
+	private int b = 0;
+	private int c = 0;
+	private int d = 4;
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
